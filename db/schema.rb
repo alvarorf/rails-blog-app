32104a0aae6935e079231f3ec88ea4fb03b7419e
alvaro <aaruizf@unal.edu.co>
2020-10-30 10:14:58 -0500

    Add styles.css.scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_222435) do
+ActiveRecord::Schema.define(version: 2020_10_30_144005) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
